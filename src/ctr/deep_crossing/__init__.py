2b36398fc18ad6107d2b8a407c6ae33817ac7c79
littlemesie <littlemesie@163.com>
2021-08-16 19:38:24 +0800

    deep crossing

---
+++ src/ctr/deep_crossing/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+"""
+@ide: PyCharm
+@author: mesie
+@date: 2021/8/16 下午7:19
+@summary:
+"""
--- src/ctr/layers/modules.py
+++ src/ctr/layers/modules.py
@@ -48,6 +48,46 @@ class FM(Layer):
         return first_order + second_order
 
 
+class CrossNetwork(Layer):
+    def __init__(self, layer_num, reg_w=1e-6, reg_b=1e-6):
+        """CrossNetwork
+        :param layer_num: A scalar. The depth of cross network
+        :param reg_w: A scalar. The regularizer of w
+        :param reg_b: A scalar. The regularizer of b
+        """
+        super(CrossNetwork, self).__init__()
+        self.layer_num = layer_num
+        self.reg_w = reg_w
+        self.reg_b = reg_b
+
+    def build(self, input_shape):
+        dim = int(input_shape[-1])
+        self.cross_weights = [
+            self.add_weight(name='w_' + str(i),
+                            shape=(dim, 1),
+                            initializer='random_normal',
+                            regularizer=l2(self.reg_w),
+                            trainable=True
+                            )
+            for i in range(self.layer_num)]
+        self.cross_bias = [
+            self.add_weight(name='b_' + str(i),
+                            shape=(dim, 1),
+                            initializer='random_normal',
+                            regularizer=l2(self.reg_b),
+                            trainable=True
+                            )
+            for i in range(self.layer_num)]
+
+    def call(self, inputs, **kwargs):
+        x_0 = tf.expand_dims(inputs, axis=2)  # (batch_size, dim, 1)
+        x_l = x_0  # (None, dim, 1)
+        for i in range(self.layer_num):
+            x_l1 = tf.tensordot(x_l, self.cross_weights[i], axes=[1, 0])  # (batch_size, dim, dim)
+            x_l = tf.matmul(x_0, x_l1) + self.cross_bias[i] + x_l  # (batch_size, dim, 1)
+        x_l = tf.squeeze(x_l, axis=2)  # (batch_size, dim)
+        return x_l
+
 class DNN(Layer):
     """
     Deep part
