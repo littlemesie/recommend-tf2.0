afdd562d98e5c46a23ded2f0921131831fd9ca90
littlemesie <littlemesie@163.com>
2021-11-25 11:26:32 +0800

    din

--- src/ctr/din/model.py
+++ src/ctr/din/model.py
@@ -7,56 +7,40 @@
 @summary:
 """
 import tensorflow as tf
-
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Embedding, Dense, BatchNormalization, Input, PReLU, Dropout
 from tensorflow.keras.regularizers import l2
-
 from ctr.layers.modules import AttentionLayer, Dice
 
 
 class DIN(Model):
-    def __init__(self, feature_columns, behavior_feature_list, att_hidden_units=(80, 40),
-                 ffn_hidden_units=(80, 40), att_activation='prelu', ffn_activation='prelu', maxlen=40, dnn_dropout=0.,
+    def __init__(self, sparse_feature_dict, sparse_feature_index, att_hidden_units=(80, 40),
+                 ffn_hidden_units=(80, 40), att_activation='prelu', ffn_activation='prelu', maxlen=10, dnn_dropout=0.,
                  embed_reg=1e-4):
         """
         DIN
-        :param feature_columns: A list. dense_feature_columns + sparse_feature_columns
-        :param behavior_feature_list: A list. the list of behavior feature names
+        :param sparse_feature_dict: A dict. sparse_feature_columns
+        :param sparse_feature_index: A dict.  sparse_feature_index
         :param att_hidden_units: A tuple or list. Attention hidden units.
         :param ffn_hidden_units: A tuple or list. Hidden units list of FFN.
         :param att_activation: A String. The activation of attention.
         :param ffn_activation: A String. Prelu or Dice.
         :param maxlen: A scalar. Maximum sequence length.
-        :param dropout: A scalar. The number of Dropout.
+        :param dnn_dropout: A scalar. The number of Dropout.
         :param embed_reg: A scalar. The regularizer of embedding.
         """
         super(DIN, self).__init__()
         self.maxlen = maxlen
-
-        self.dense_feature_columns, self.sparse_feature_columns = feature_columns
-
-        # len
-        self.other_sparse_len = len(self.sparse_feature_columns) - len(behavior_feature_list)
-        self.dense_len = len(self.dense_feature_columns)
-        self.behavior_num = len(behavior_feature_list)
-
-        # other embedding layers
-        self.embed_sparse_layers = [Embedding(input_dim=feat['feat_num'],
-                                              input_length=1,
-                                              output_dim=feat['embed_dim'],
-                                              embeddings_initializer='random_uniform',
-                                              embeddings_regularizer=l2(embed_reg))
-                                    for feat in self.sparse_feature_columns
-                                    if feat['feat'] not in behavior_feature_list]
-        # behavior embedding layers, item id and category id
-        self.embed_seq_layers = [Embedding(input_dim=feat['feat_num'],
-                                           input_length=1,
-                                           output_dim=feat['embed_dim'],
-                                           embeddings_initializer='random_uniform',
-                                           embeddings_regularizer=l2(embed_reg))
-                                 for feat in self.sparse_feature_columns
-                                 if feat['feat'] in behavior_feature_list]
+        self.sparse_feature_dict = sparse_feature_dict
+        self.user_sparse_feature_index, self.item_sparse_feature_index, self.behavior_feature_index = sparse_feature_index
+        self.embed_layers = {
+            'embed_' + k: Embedding(input_dim=v[0],
+                                    input_length=1,
+                                    output_dim=v[1],
+                                    embeddings_initializer='random_uniform',
+                                    embeddings_regularizer=l2(embed_reg))
+            for k, v in self.sparse_feature_dict.items()
+        }
 
         # attention layer
         self.attention_layer = AttentionLayer(att_hidden_units, att_activation)
@@ -66,61 +50,56 @@ class DIN(Model):
         self.ffn = [Dense(unit, activation=PReLU() if ffn_activation == 'prelu' else Dice()) \
                     for unit in ffn_hidden_units]
         self.dropout = Dropout(dnn_dropout)
-        self.dense_final = Dense(1)
+        self.final_output = Dense(1)
 
     def call(self, inputs, **kwargs):
-        # dense_inputs and sparse_inputs is empty
-        # seq_inputs (None, maxlen, behavior_num)
-        # item_inputs (None, behavior_num)
-        dense_inputs, sparse_inputs, seq_inputs, item_inputs = inputs
-        # attention ---> mask, if the element of seq_inputs is equal 0, it must be filled in.
-        mask = tf.cast(tf.not_equal(seq_inputs[:, :, 0], 0), dtype=tf.float32)  # (None, maxlen)
-        # other
-        other_info = dense_inputs
-        for i in range(self.other_sparse_len):
-            other_info = tf.concat([other_info, self.embed_sparse_layers[i](sparse_inputs[:, i])], axis=-1)
 
-        # seq, item embedding and category embedding should concatenate
-        seq_embed = tf.concat([self.embed_seq_layers[i](seq_inputs[:, :, i]) for i in range(self.behavior_num)],
-                              axis=-1)
-        item_embed = tf.concat([self.embed_seq_layers[i](item_inputs[:, i]) for i in range(self.behavior_num)], axis=-1)
+        user_dense_input, user_sparse_input, item_dense_input, item_sparse_input, behavior_input = inputs
+        print(behavior_input)
+        print(self.user_sparse_feature_index)
+        # user embed
+        user_embeddings = tf.concat([self.embed_layers['embed_{}'.format(k)](user_sparse_input[:, v])
+                                     for k, v in self.user_sparse_feature_index.items()], axis=-1)
+        user_embed = tf.concat([user_dense_input, user_embeddings], axis=-1)
+        # item embed
+        item_embeddings = tf.concat([self.embed_layers['embed_{}'.format(k)](item_sparse_input[:, v])
+                                     for k, v in self.item_sparse_feature_index.items()], axis=-1)
+        item_embed = tf.concat([item_sparse_input, item_embeddings], axis=-1)
+
+        # attention ---> mask
+        mask = tf.concat([behavior_input[:, i*3] for i in range(self.maxlen)], axis=-1)
+
+        # behavior embed
+        behavior_embed = tf.concat([self.embed_layers['embed_{}'.format(f"{k.split('_')[0]}_{k.split('_')[1]}_{k.split('_')[3]}")]
+                           (behavior_input[:, v]) for k, v in self.behavior_feature_index.items()], axis=-1)
+
+        behavior_embed = tf.reshape(behavior_embed, shape=(-1, self.maxlen, item_embeddings.shape[1]))
 
         # att
-        user_info = self.attention_layer([item_embed, seq_embed, seq_embed, mask])  # (None, d * 2)
+        att_outputs = self.attention_layer([item_embeddings, behavior_embed, behavior_embed, mask])
 
-        # concat user_info(att hist), cadidate item embedding, other features
-        if self.dense_len > 0 or self.other_sparse_len > 0:
-            info_all = tf.concat([user_info, item_embed, other_info], axis=-1)
-        else:
-            info_all = tf.concat([user_info, item_embed], axis=-1)
+        all_inputs = tf.concat([user_embed, item_embed, att_outputs], axis=-1)
 
-        info_all = self.bn(info_all)
+        all_inputs = self.bn(all_inputs)
 
         # ffn
         for dense in self.ffn:
-            info_all = dense(info_all)
+            all_inputs = dense(all_inputs)
 
-        info_all = self.dropout(info_all)
-        outputs = tf.nn.sigmoid(self.dense_final(info_all))
+        all_inputs = self.dropout(all_inputs)
+        #
+        outputs = tf.nn.sigmoid(self.final_output(all_inputs))
+        print(outputs)
+        # outputs = Dense(1, activation='sigmoid', name='output')(all_inputs)
         return outputs
 
-    def summary(self, **kwargs):
-        dense_inputs = Input(shape=(self.dense_len,), dtype=tf.float32)
-        sparse_inputs = Input(shape=(self.other_sparse_len,), dtype=tf.int32)
-        seq_inputs = Input(shape=(self.maxlen, self.behavior_num), dtype=tf.int32)
-        item_inputs = Input(shape=(self.behavior_num,), dtype=tf.int32)
-        tf.keras.Model(inputs=[dense_inputs, sparse_inputs, seq_inputs, item_inputs],
-                       outputs=self.call([dense_inputs, sparse_inputs, seq_inputs, item_inputs])).summary()
+    def build_graph(self, **kwargs):
+        user_dense_input = Input(shape=(5,))
+        user_sparse_input = Input(shape=(3,))
+        item_dense_input = Input(shape=(5,))
+        item_sparse_input = Input(shape=(3,))
+        behavior_input = Input(shape=(3*self.maxlen,))
 
-
-def test_model():
-    dense_features = [{'feat': 'a'}, {'feat': 'b'}]
-    sparse_features = [{'feat': 'item_id', 'feat_num': 100, 'embed_dim': 8},
-                       {'feat': 'cate_id', 'feat_num': 100, 'embed_dim': 8},
-                       {'feat': 'adv_id', 'feat_num': 100, 'embed_dim': 8}]
-    behavior_list = ['item_id', 'cate_id']
-    features = [dense_features, sparse_features]
-    model = DIN(features, behavior_list)
-    model.summary()
-
-# test_model()
+        model = Model(inputs=[user_dense_input, user_sparse_input, item_dense_input, item_sparse_input, behavior_input],
+                       outputs=self.call([user_dense_input, user_sparse_input, item_dense_input, item_sparse_input, behavior_input]))
+        return model
--- src/ctr/din/train.py
+++ src/ctr/din/train.py
@@ -6,6 +6,8 @@
 @date: 2021/7/31 下午3:44
 @summary:
 """
+import pandas as pd
+import numpy as np
 import tensorflow as tf
 from time import time
 from tensorflow.keras.losses import binary_crossentropy
@@ -20,30 +22,78 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['CUDA_VISIBLE_DEVICES'] = '6'
+maxlen = 10
 
-if __name__ == '__main__':
-    # ========================= Hyper Parameters =======================
+embed_dim = 8
+att_hidden_units = [80, 40]
+ffn_hidden_units = [256, 128, 64]
+dnn_dropout = 0.5
+att_activation = 'sigmoid'
+ffn_activation = 'prelu'
 
-    maxlen = 20
+learning_rate = 0.001
+batch_size = 64
+epochs = 5
 
-    embed_dim = 8
-    att_hidden_units = [80, 40]
-    ffn_hidden_units = [256, 128, 64]
-    dnn_dropout = 0.5
-    att_activation = 'sigmoid'
-    ffn_activation = 'prelu'
+user_dense_feature_train = pd.DataFrame(np.random.random((10000, 5)),
+                                                columns=['user_dense_{}'.format(i) for i in range(5)])
+user_sparse_feature_train = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['user_sparse_{}'.format(i) for i in range(3)])
+item_dense_feature_train = pd.DataFrame(np.random.random((10000, 5)),
+                                                columns=['item_dense_{}'.format(i) for i in range(5)])
+item_sparse_feature_train = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['item_sparse_{}'.format(i) for i in range(3)])
+behavior_feature_train = None
+for ml in range(maxlen):
+    tmp = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['item_sparse_{}_{}'.format(ml, i) for i in range(3)])
+    if ml == 0:
+        behavior_feature_train = tmp
+    else:
+        behavior_feature_train = pd.concat([behavior_feature_train, tmp], axis=1)
 
-    learning_rate = 0.001
-    batch_size = 512
-    epochs = 5
-    # ========================== Create dataset =======================
-    feature_columns, behavior_list, train, val, test = create_amazon_electronic_dataset( embed_dim, maxlen)
-    train_X, train_y = train
-    val_X, val_y = val
-    test_X, test_y = test
-    # ============================Build Model==========================
-    model = DIN(feature_columns, behavior_list, att_hidden_units, ffn_hidden_units, att_activation,
-                ffn_activation, maxlen, dnn_dropout)
+target_train = pd.DataFrame(np.random.randint(0, 2, size=10000))
+
+# valid
+user_dense_feature_val = pd.DataFrame(np.random.random((10000, 5)),
+                                                columns=['user_dense_{}'.format(i) for i in range(5)])
+user_sparse_feature_val = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['user_sparse_{}'.format(i) for i in range(3)])
+item_dense_feature_val = pd.DataFrame(np.random.random((10000, 5)),
+                                                columns=['item_dense_{}'.format(i) for i in range(5)])
+item_sparse_feature_val = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['item_sparse_{}'.format(i) for i in range(3)])
+behavior_feature_val = None
+for ml in range(maxlen):
+    tmp = pd.DataFrame(np.random.randint(0, 10, size=(10000, 3)),
+                                           columns=['item_sparse_{}_{}'.format(ml, i) for i in range(3)])
+    if ml == 0:
+        behavior_feature_val = tmp
+    else:
+        behavior_feature_val = pd.concat([behavior_feature_val, tmp], axis=1)
+
+target_val = pd.DataFrame(np.random.randint(0, 2, size=10000))
+
+sparse_feature_dict = {}
+user_sparse_feature_index = {}
+item_sparse_feature_index = {}
+behavior_feature_index = {}
+
+for idx, col in enumerate(user_sparse_feature_train.columns):
+    sparse_feature_dict[col] = (user_sparse_feature_train[col].max() + 1, 64)
+    user_sparse_feature_index[col] = idx
+for idx, col in enumerate(item_sparse_feature_train.columns):
+    sparse_feature_dict[col] = (item_sparse_feature_train[col].max() + 1, 64)
+    item_sparse_feature_index[col] = idx
+for idx, col in enumerate(behavior_feature_train.columns):
+    behavior_feature_index[col] = idx
+
+sparse_feature_index = [user_sparse_feature_index, item_sparse_feature_index, behavior_feature_index]
+mirrored_strategy = tf.distribute.MirroredStrategy()
+with mirrored_strategy.scope():
+    din = DIN(sparse_feature_dict, sparse_feature_index, att_hidden_units, ffn_hidden_units, att_activation,
+                    ffn_activation, maxlen, dnn_dropout)
+    model = din.build_graph()
     model.summary()
     # ============================model checkpoint======================
     # check_path = 'save/din_weights.epoch_{epoch:04d}.val_loss_{val_loss:.4f}.ckpt'
@@ -52,14 +102,11 @@ if __name__ == '__main__':
     # =========================Compile============================
     model.compile(loss=binary_crossentropy, optimizer=Adam(learning_rate=learning_rate),
                   metrics=[AUC()])
-    # ===========================Fit==============================
-    model.fit(
-        train_X,
-        train_y,
-        epochs=epochs,
-        # callbacks=[EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)],  # checkpoint
-        validation_data=(val_X, val_y),
-        batch_size=batch_size,
-    )
-    # ===========================Test==============================
-    print('test AUC: %f' % model.evaluate(test_X, test_y, batch_size=batch_size)[1])
+    model.run_eagerly = True
+model.fit([user_dense_feature_train, user_sparse_feature_train, item_dense_feature_train, item_sparse_feature_train, behavior_feature_train],
+            target_train,
+            epochs=epochs,
+            callbacks=[EarlyStopping(monitor='val_loss', patience=1, restore_best_weights=True)],  # checkpoint
+            validation_split=0,
+            batch_size=batch_size,
+          )
