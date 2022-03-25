c682a5c3f0ba8261c4b477523ed3274cfec7b4db
littlemesie <littlemesie@163.com>
2022-03-25 17:43:29 +0800

    sasrec

--- src/match/layers/modules.py
+++ src/match/layers/modules.py
@@ -60,86 +60,84 @@ class SampledSoftmaxLayer(Layer):
                                           )
         return tf.expand_dims(loss, axis=1)
 
-def scaled_dot_product_attention(q, k, v, mask, causality=True):
+def split_heads(x, seq_len, num_heads, depth):
+    """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+    Args:
+        :param x: A Tensor with shape of [batch_size, seq_len, num_heads * depth]
+        :param seq_len: A scalar(int).
+        :param num_heads: A scalar(int).
+        :param depth: A scalar(int).
+    :return: A tensor with shape of [batch_size, num_heads, seq_len, depth]
     """
-    Attention Mechanism
-    :param q: A 3d tensor with shape of (None, seq_len, depth), depth = d_model // num_heads
-    :param k: A 3d tensor with shape of (None, seq_len, depth)
-    :param v: A 3d tensor with shape of (None, seq_len, depth)
-    :param mask:
-    :param causality: Boolean. If True, using causality, default True
+    x = tf.reshape(x, (-1, seq_len, num_heads, depth))
+    return tf.transpose(x, perm=[0, 2, 1, 3])
+
+def scaled_dot_product_attention(q, k, v, mask):
+    """Attention Mechanism Function.
+    Args:
+        :param q: A 3d/4d tensor with shape of (None, ..., seq_len, dim)
+        :param k: A 3d/4d tensor with shape of (None, ..., seq_len, dim)
+        :param v: A 3d/4d tensor with shape of (None, ..., seq_len, dim)
+        :param mask: A 3d/4d tensor with shape of (None, ..., seq_len, 1)
     :return:
     """
     mat_qk = tf.matmul(q, k, transpose_b=True)  # (None, seq_len, seq_len)
-    dk = tf.cast(k.shape[-1], dtype=tf.float32)
     # Scaled
+    dk = tf.cast(k.shape[-1], dtype=tf.float32)
     scaled_att_logits = mat_qk / tf.sqrt(dk)
 
-    paddings = tf.ones_like(scaled_att_logits) * (-2 ** 32 + 1)
-    outputs = tf.where(tf.equal(mask, 0), paddings, scaled_att_logits)  # (None, seq_len, seq_len)
-
-    # Causality
-    if causality:
-        diag_vals = tf.ones_like(outputs)  # (None, seq_len, seq_len)
-        masks = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()  # (None, seq_len, seq_len)
-
-        paddings = tf.ones_like(masks) * (-2 ** 32 + 1)
-        outputs = tf.where(tf.equal(masks, 0), paddings, outputs)  # (None, seq_len, seq_len)
-
+    paddings = tf.ones_like(scaled_att_logits) * (-2 ** 32 + 1)  # (None, seq_len, seq_len)
+    outputs = tf.where(tf.equal(mask, tf.zeros_like(mask)), paddings, scaled_att_logits)  # (None, seq_len, seq_len)
     # softmax
     outputs = tf.nn.softmax(logits=outputs)  # (None, seq_len, seq_len)
-    outputs = tf.matmul(outputs, v)  # (None, seq_len, depth)
+    outputs = tf.matmul(outputs, v)  # (None, seq_len, dim)
 
     return outputs
 
-
 class MultiHeadAttention(Layer):
-    def __init__(self, d_model, num_heads, causality=True):
-        """
-        Multi Head Attention Mechanism
-        :param d_model: A scalar. The self-attention hidden size.
-        :param num_heads: A scalar. Number of heads. If num_heads == 1, the layer is a single self-attention layer.
-        :param causality: Boolean. If True, using causality, default True
-
+    def __init__(self, d_model, num_heads):
+        """Multi Head Attention Mechanism.
+        Args:
+            :param d_model: A scalar. The self-attention hidden size.
+            :param num_heads: A scalar. Number of heads. If num_heads == 1, the layer is a single self-attention layer.
+        :return:
         """
         super(MultiHeadAttention, self).__init__()
         self.d_model = d_model
         self.num_heads = num_heads
-        self.causality = causality
 
         self.wq = Dense(d_model, activation=None)
         self.wk = Dense(d_model, activation=None)
         self.wv = Dense(d_model, activation=None)
 
-    def call(self, inputs, **kwargs):
-        q, k, v, mask = inputs
+
+    def call(self, q, k, v, mask):
+        # q, k, v, mask = inputs
         q = self.wq(q)  # (None, seq_len, d_model)
         k = self.wk(k)  # (None, seq_len, d_model)
         v = self.wv(v)  # (None, seq_len, d_model)
-
-        # split d_model into num_heads * depth, and concatenate
-        q = tf.reshape(tf.concat([tf.split(q, self.num_heads, axis=2)], axis=0),
-                       (-1, q.shape[1], q.shape[2] // self.num_heads))  # (None * num_heads, seq_len, d_model // num_heads)
-        k = tf.reshape(tf.concat([tf.split(k, self.num_heads, axis=2)], axis=0),
-                       (-1, k.shape[1], k.shape[2] // self.num_heads))  # (None * num_heads, seq_len, d_model // num_heads)
-        v = tf.reshape(tf.concat([tf.split(v, self.num_heads, axis=2)], axis=0),
-                       (-1, v.shape[1], v.shape[2] // self.num_heads))  # (None * num_heads, seq_len, d_model // num_heads)
-
+        # split d_model into num_heads * depth
+        seq_len, d_model = q.shape[1], q.shape[2]
+        q = split_heads(q, seq_len, self.num_heads, q.shape[2] // self.num_heads)  # (None, num_heads, seq_len, depth)
+        k = split_heads(k, seq_len, self.num_heads, k.shape[2] // self.num_heads)  # (None, num_heads, seq_len, depth)
+        v = split_heads(v, seq_len, self.num_heads, v.shape[2] // self.num_heads)  # (None, num_heads, seq_len, depth)
+        # mask
+        mask = tf.tile(tf.expand_dims(mask, axis=1), [1, self.num_heads, 1, 1])  # (None, num_heads, seq_len, 1)
         # attention
-        scaled_attention = scaled_dot_product_attention(q, k, v, mask, self.causality)  # (None * num_heads, seq_len, d_model // num_heads)
-
-        # Reshape
-        outputs = tf.concat(tf.split(scaled_attention, self.num_heads, axis=0), axis=2)  # (N, seq_len, d_model)
-
+        scaled_attention = scaled_dot_product_attention(q, k, v, mask)  # (None, num_heads, seq_len, d_model // num_heads)
+        # reshape
+        outputs = tf.reshape(tf.transpose(scaled_attention, [0, 2, 1, 3]), [-1, seq_len, d_model])  # (None, seq_len, d_model)
         return outputs
 
 
 class FFN(Layer):
     def __init__(self, hidden_unit, d_model):
-        """
-        Feed Forward Network
-        :param hidden_unit: A scalar. W1
-        :param d_model: A scalar. W2
+        """Feed Forward Network.
+        Args:
+            :param hidden_unit: A scalar.
+            :param d_model: A scalar.
+        :return:
         """
         super(FFN, self).__init__()
         self.conv1 = Conv1D(filters=hidden_unit, kernel_size=1, activation='relu', use_bias=True)
@@ -151,23 +149,23 @@ class FFN(Layer):
         return output
 
 
-class EncoderLayer(Layer):
-    def __init__(self, d_model, num_heads=1, ffn_hidden_unit=128, dropout=0., norm_training=True, causality=True):
+class TransformerEncoder(Layer):
+    def __init__(self, d_model, num_heads=1, ffn_hidden_unit=128, dropout=0., layer_norm_eps=1e-6):
+        """Encoder Layer.
+        Args:
+            :param d_model: A scalar. The self-attention hidden size.
+            :param num_heads: A scalar. Number of heads.
+            :param ffn_hidden_unit: A scalar. Number of hidden unit in FFN
+            :param dropout: A scalar. Number of dropout.
+            :param layer_norm_eps: A scalar. Small float added to variance to avoid dividing by zero.
+        :return:
         """
-        Encoder Layer
-        :param d_model: A scalar. The self-attention hidden size.
-        :param num_heads: A scalar. Number of heads.
-        :param ffn_hidden_unit: A scalar. Number of hidden unit in FFN
-        :param dropout: A scalar. Number of dropout.
-        :param norm_training: Boolean. If True, using layer normalization, default True
-        :param causality: Boolean. If True, using causality, default True
-        """
-        super(EncoderLayer, self).__init__()
-        self.mha = MultiHeadAttention(d_model, num_heads, causality)
+        super(TransformerEncoder, self).__init__()
+        self.mha = MultiHeadAttention(d_model, num_heads)
         self.ffn = FFN(ffn_hidden_unit, d_model)
 
-        self.layernorm1 = LayerNormalization(epsilon=1e-6, trainable=norm_training)
-        self.layernorm2 = LayerNormalization(epsilon=1e-6, trainable=norm_training)
+        self.layernorm1 = LayerNormalization(epsilon=layer_norm_eps)
+        self.layernorm2 = LayerNormalization(epsilon=layer_norm_eps)
 
         self.dropout1 = Dropout(dropout)
         self.dropout2 = Dropout(dropout)
@@ -175,10 +173,10 @@ class EncoderLayer(Layer):
     def call(self, inputs, **kwargs):
         x, mask = inputs
         # self-attention
-        att_out = self.mha([x, x, x, mask])  # （None, seq_len, d_model)
+        att_out = self.mha(x, x, x, mask)  # (None, seq_len, d_model)
         att_out = self.dropout1(att_out)
         # residual add
-        out1 = self.layernorm1(x + att_out)
+        out1 = self.layernorm1(x + att_out)  # (None, seq_len, d_model)
         # ffn
         ffn_out = self.ffn(out1)
         ffn_out = self.dropout2(ffn_out)
--- src/match/sasrec/model.py
+++ src/match/sasrec/model.py
@@ -10,33 +10,29 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Dropout, Embedding, Input
-from match.layers.modules import EncoderLayer
+from match.layers.modules import TransformerEncoder
 
 
-class SASRec(tf.keras.Model):
+class SASRec(Model):
     def __init__(self, user_sparse_feature_columns, item_sparse_feature_columns,
                  user_dense_feature_columns=(), item_dense_feature_columns=(),
-                 user_var_sparse_feature_columns=(), item_var_sparse_feature_columns=(),
-                 blocks=1, num_heads=1, ffn_hidden_unit=128,
-                 dropout=0., maxlen=20, norm_training=True, causality=False, embed_reg=1e-6):
+                 blocks=1, num_heads=1, att_hidden_unit=128, ffn_hidden_unit=128,
+                 dnn_dropout=0., layer_norm_eps=1e-6, seq_len=10, neg_len=100, embed_reg=1e-6):
         """
         SASRec model
         """
         super(SASRec, self).__init__()
         # sequence length
-        self.maxlen = maxlen
+        self.seq_len = seq_len
+        self.neg_len = neg_len
         # user item feature columns
         self.user_sparse_feature_columns = user_sparse_feature_columns
         self.user_dense_feature_columns = user_dense_feature_columns
         self.item_sparse_feature_columns = item_sparse_feature_columns
         self.item_dense_feature_columns = item_dense_feature_columns
-        self.user_var_sparse_feature_columns = user_var_sparse_feature_columns
-        self.item_var_sparse_feature_columns = item_var_sparse_feature_columns
 
-        # embed_dim
-        self.embed_dim = self.item_fea_col['embed_dim']
-        # d_model must be the same as embedding_dim, because of residual connection
-        self.d_model = self.embed_dim
+        # d_model must be the same as seq_embed_dim, because of residual connection
+        self.d_model = att_hidden_unit
 
         self.user_embed_layers = {
             'embed_' + str(feat['feat']): Embedding(input_dim=feat['feat_num'],
@@ -55,69 +51,82 @@ class SASRec(tf.keras.Model):
                                                     embeddings_regularizer=l2(embed_reg))
             for feat in self.item_sparse_feature_columns
         }
-        self.dropout = Dropout(dropout)
+
+        self.dropout = Dropout(dnn_dropout)
         # attention block
-        self.encoder_layer = [EncoderLayer(self.d_model, num_heads, ffn_hidden_unit,
-                                           dropout, norm_training, causality) for b in range(blocks)]
+        self.encoder_layer = [TransformerEncoder(self.d_model, num_heads, ffn_hidden_unit,
+                                                 dnn_dropout, layer_norm_eps) for _ in range(blocks)]
 
     def call(self, inputs, **kwargs):
         # inputs
-        seq_inputs, pos_inputs, neg_inputs = inputs  # (None, maxlen), (None, 1), (None, 1)
-        # mask
-        mask = tf.expand_dims(tf.cast(tf.not_equal(seq_inputs, 0), dtype=tf.float32), axis=-1)  # (None, maxlen, 1)
-        # seq info
-        seq_embed = self.item_embedding(seq_inputs)  # (None, maxlen, dim)
-        # pos encoding
-        # pos_encoding = positional_encoding(seq_inputs, self.embed_dim)
-        pos_encoding = tf.expand_dims(self.pos_embedding(tf.range(self.maxlen)), axis=0)
-        seq_embed += pos_encoding
-        seq_embed = self.dropout(seq_embed)
-        att_outputs = seq_embed  # (None, maxlen, dim)
-        att_outputs *= mask
+        seq_inputs, pos_inputs, neg_inputs = inputs
+        # # user embed
+        # user_sparse_embed = tf.concat([self.user_embed_layers['embed_{}'.format(k)](v)
+        #                                for k, v in user_sparse_inputs.items()], axis=-1)
+        # print(user_sparse_embed)
+        # # item embed
+        # item_sparse_embed = tf.concat([self.item_embed_layers['embed_{}'.format(k)](v)
+        #                                for k, v in item_sparse_inputs.items()], axis=-1)
 
+        # mask
+        mask = tf.expand_dims(tf.cast(tf.not_equal(seq_inputs, 0), dtype=tf.float32), axis=-1)
+
+        # seq embed 没加positional embed
+        seq_embed = self.user_embed_layers['embed_seq_item'](seq_inputs)
+        # pos embed
+        pos_embed = self.user_embed_layers['embed_pos_item'](pos_inputs)
+        # neg embed
+        neg_embed = self.user_embed_layers['embed_neg_item'](neg_inputs)
+
+        att_outputs = seq_embed  # (None, seq_len, embed_dim)
+        att_outputs *= mask
         # self-attention
         for block in self.encoder_layer:
             att_outputs = block([att_outputs, mask])  # (None, seq_len, dim)
             att_outputs *= mask
 
-        # user_info = tf.reduce_mean(att_outputs, axis=1)  # (None, dim)
-        user_info = tf.expand_dims(att_outputs[:, -1], axis=1)  # (None, 1, dim)
-        # item info
-        pos_info = self.item_embedding(pos_inputs)  # (None, 1, dim)
-        neg_info = self.item_embedding(neg_inputs)  # (None, 1/100, dim)
-        pos_logits = tf.reduce_sum(user_info * pos_info, axis=-1)  # (None, 1)
-        neg_logits = tf.reduce_sum(user_info * neg_info, axis=-1)  # (None, 1)
-        # loss
-        losses = tf.reduce_mean(- tf.math.log(tf.nn.sigmoid(pos_logits)) -
-                                tf.math.log(1 - tf.nn.sigmoid(neg_logits))) / 2
+        seq_info = tf.expand_dims(att_outputs[:, -1], axis=1)  # (None, 1, dim)
+        self.embed = seq_info
+        pos_scores = tf.reduce_sum(tf.multiply(seq_info, pos_embed), axis=-1)  # (None, 1)
+        neg_scores = tf.reduce_sum(seq_info * neg_embed, axis=-1)  # (None, 1)
+        # "binary entropy loss
+        losses = tf.reduce_mean(- tf.math.log(tf.nn.sigmoid(pos_scores)) -
+                                tf.math.log(1 - tf.nn.sigmoid(neg_scores))) / 2
         self.add_loss(losses)
-        logits = tf.concat([pos_logits, neg_logits], axis=-1)
+        logits = tf.concat([pos_scores, neg_scores], axis=-1)
         return logits
 
-    def summary(self,  **kwargs):
+    def build_graph(self, **kwargs):
         user_sparse_inputs = {uf['feat']: Input(shape=(1,), dtype=tf.float32) for uf in
                               self.user_sparse_feature_columns}
         item_sparse_inputs = {uf['feat']: Input(shape=(1,), dtype=tf.float32) for uf in
                               self.item_sparse_feature_columns}
 
-        seq_inputs = Input(shape=(self.maxlen,), dtype=tf.int32)
+        seq_inputs = Input(shape=(self.seq_len,), dtype=tf.int32)
+        pos_inputs = Input(shape=(1,), dtype=tf.int32)
+        neg_inputs = Input(shape=(self.neg_len,), dtype=tf.int32)
 
-        model = Model(inputs=[seq_inputs, user_sparse_inputs, item_sparse_inputs],
-                      outputs=self.call([seq_inputs, user_sparse_inputs, item_sparse_inputs]))
+        model = Model(inputs=[seq_inputs, pos_inputs, neg_inputs],
+                      outputs=self.call([seq_inputs, pos_inputs, neg_inputs]))
 
-        model.__setattr__("user_sparse_input", user_sparse_inputs)
-        model.__setattr__("item_sparse_input", item_sparse_inputs)
-        model.__setattr__("user_embeding", self.user_dnn_out)
-        model.__setattr__("item_embeding", self.item_dnn_out)
+        user_input = user_sparse_inputs.update({'seq_item': seq_inputs})
+
+        model.__setattr__("user_input", user_input)
+        model.__setattr__("item_input", item_sparse_inputs)
+        model.__setattr__("embed", self.embed)
         return model
 
 
 
-def model_test():
-    item_fea_col = {'feat': 'item_id', 'feat_num': 100, 'embed_dim': 8}
-    model = SASRec(item_fea_col, num_heads=8)
-    m = model.summary()
-    m.summary()
-
-
-model_test()
+# def model_test():
+#     user_features = [{'feat': 'user_id', 'feat_num': 100, 'feat_len': 1, 'embed_dim': 8},
+#                      {'feat': 'seq_item', 'feat_num': 100, 'feat_len': 10, 'embed_dim': 64},
+#                      {'feat': 'pos_item', 'feat_num': 100, 'feat_len': 1, 'embed_dim': 64},
+#                      {'feat': 'neg_item', 'feat_num': 100, 'feat_len': 100, 'embed_dim': 64}]
+#     item_features = [{'feat': 'item_id', 'feat_num': 100, 'feat_len': 1, 'embed_dim': 32}]
+#     model = SASRec(user_features, item_features, att_hidden_unit=64)
+#     m = model.build_graph()
+#     m.summary()
+#
+#
+# model_test()
